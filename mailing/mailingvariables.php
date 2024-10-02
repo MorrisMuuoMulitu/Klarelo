3a89968f88e232aaa1c4ed0927db5fb57a7a9b5b
Morris Mulitu <morrismulitu@gmail.com>
2024-10-02 14:53:30 +0300

    updating the mailing server

--- mailing/mailingvariables.php
+++ mailing/mailingvariables.php
@@ -1,7 +1,7 @@
 <?php
-    $mail_host = "smtp.gmail.com";
-    $mail_port = "587";
-    $mail_sender_email = ""; //sender
-    $mail_sender_password = ""; //sender
-    $mail_sender_name = "Website Form";
+$mail_host = "smtp.gmail.com";
+$mail_port = "587";
+$mail_sender_email = "morrismulitu@gmail.com"; //sender
+$mail_sender_password = "12April@Muuo"; //sender
+$mail_sender_name = "Website Form";
 ?>
