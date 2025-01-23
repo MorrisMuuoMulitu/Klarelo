cacbd2f165009f11360e229bfebfc1cd460a56df
Morris Mulitu <morrismulitu@gmail.com>
2025-01-23 17:30:02 +0300

    removed php, code optimised

--- mailing/mailingvariables.php
+++
@@ -1,7 +0,0 @@
-<?php
-$mail_host = "smtp.gmail.com";
-$mail_port = "587";
-$mail_sender_email = "morrismulitu@gmail.com"; //sender
-$mail_sender_password = "12April@Muuo"; //sender
-$mail_sender_name = "Website Form";
-?>
